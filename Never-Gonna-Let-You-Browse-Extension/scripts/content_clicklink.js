20bf15905c525b5b05e47655fe8d915ff2bfbb14
Ellie <ellie_neufeld@sfu.ca>
2025-03-08 14:17:10 -0800

    changes link randomly based on a fixed rate

--- Never-Gonna-Let-You-Browse-Extension/scripts/content_clicklink.js
+++ Never-Gonna-Let-You-Browse-Extension/scripts/content_clicklink.js
@@ -6,7 +6,10 @@ document.addEventListener('auxclick', function(e){ replaceLink(e); });
 
 function replaceLink(e) {
     //alert(e.target);
-    if (e.target.matches('a')) {
-        e.target.setAttribute('href', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+    const bool = Math.random() < 0.25 // 25% chance
+    if (bool) {
+        if (e.target.matches('a')) {
+            e.target.setAttribute('href', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+        }
     }
 }
