467232b6d957f42bbec2433a539d5103bc25fa6e
Nathan Huynh <nathanhuynh171005@gmail.com>
2025-03-08 20:24:36 -0800

    semi change image

--- Never-Gonna-Let-You-Browse-Extension/scripts/change_image.js
+++ Never-Gonna-Let-You-Browse-Extension/scripts/change_image.js
@@ -1,3 +1,3 @@
 document.querySelectorAll("img").forEach((imageTag) => {
-    imageTag.src = "../source"
+    imageTag.src = "https://static.wikia.nocookie.net/dqw4w9wgxcq/images/0/08/Site-background-dark/revision/latest/scale-to-width-down/1400?cb=20220428173233"
 });
