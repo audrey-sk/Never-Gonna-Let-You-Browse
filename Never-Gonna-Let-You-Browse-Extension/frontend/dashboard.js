66157d33edbc02f4ba480c7a546da129382eb55c
Ellie <ellie1neufeld@gmail.com>
2025-03-08 21:43:51 -0800

    added paragraph tags to string text

--- Never-Gonna-Let-You-Browse-Extension/frontend/dashboard.js
+++ Never-Gonna-Let-You-Browse-Extension/frontend/dashboard.js
@@ -1,8 +1,10 @@
 const activateRickRoll = () => {
     var cur = chrome.storage.sync.get( { rickroll: true } );
 
-    if (cur == true) { document.getElementById("rickrolltext").innerHTML = "Let the<br>Rickrolling<br>begin!"; }
-    else { document.getElementById("rickrolltext").innerHTML = "You're safe<br>... For now"; }
+    //if (cur == true) { document.getElementById("rickrolltext").innerHTML = "Let the<br>Rickrolling<br>begin!"; }
+    //else { document.getElementById("rickrolltext").innerHTML = "You're safe<br>... For now"; }
+    if (cur == true) { document.getElementsByTagName("h2").innerHTML = "<p>Let the<br>Rickrolling<br>begin!</p>"; }
+    else { document.getElementsByTagName("h3").innerHTML = "<p>You're safe<br>... For now</p>"; }
 
     chrome.storage.sync.set( { rickroll: !cur } );
 };
@@ -10,8 +12,8 @@ const activateRickRoll = () => {
 const activateRandom = () => {
     var cur = chrome.storage.sync.get( { random: true } );
 
-    if (cur == true) { document.getElementById("randomtext").innerHTML = "Random madness"; }
-    else { document.getElementById("randomtext").innerHTML = "Constant chaos"; }
+    if (cur == true) { document.getElementById("randomtext").innerHTML = "<p>Random madness</p>"; }
+    else { document.getElementById("randomtext").innerHTML = "<p>Constant chaos</p>"; }
 
     chrome.storage.sync.set( { rickroll: !cur } );
 };
@@ -19,11 +21,11 @@ const activateRandom = () => {
 const restoreOptions = () => {
     var curRickroll, curRandom = chrome.storage.sync.get( { rickroll: true, random: true } );
 
-    if (curRickroll == true) { document.getElementById("rickrolltext").innerHTML = "Let the<br>Rickrolling<br>begin!"; }
-    else { document.getElementById("rickrolltext").innerHTML = "You're safe<br>... For now"; }
+    if (curRickroll == true) { document.getElementById("rickrolltext").innerHTML = "<p>Let the<br>Rickrolling<br>begin!</p>"; }
+    else { document.getElementById("rickrolltext").innerHTML = "<p>You're safe<br>... For now</p>"; }
 
-    if (curRandom == true) { document.getElementById("randomtext").innerHTML = "Random madness"; }
-    else { document.getElementById("randomtext").innerHTML = "Constant chaos"; }
+    if (curRandom == true) { document.getElementById("randomtext").innerHTML = "<p>Random madness</p>"; }
+    else { document.getElementById("randomtext").innerHTML = "<p>Constant chaos</p>"; }
 };
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
