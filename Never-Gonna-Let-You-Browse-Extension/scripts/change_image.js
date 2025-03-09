cf035da838bb2d2c5de8bac853ef794e7b0e1bb9
Nathan Huynh <nathanhuynh171005@gmail.com>
2025-03-09 01:12:37 -0800

    add in a bit for image

--- Never-Gonna-Let-You-Browse-Extension/scripts/change_image.js
+++ Never-Gonna-Let-You-Browse-Extension/scripts/change_image.js
@@ -1,3 +1,7 @@
 document.querySelectorAll("img").forEach((imageTag) => {
-    imageTag.src = "https://static.wikia.nocookie.net/dqw4w9wgxcq/images/0/08/Site-background-dark/revision/latest/scale-to-width-down/1400?cb=20220428173233"
+    imageTag.src = "https://static.wikia.nocookie.net/dqw4w9wgxcq/images/0/08/Site-background-dark/revision/latest/scale-to-width-down/1400?cb=20220428173233";
+    // Add CSS properties for responsive resizing
+    imageTag.style.maxWidth = "auto";
+    imageTag.style.height = "auto";
+    imageTag.style.objectFit = "contain";
 });
